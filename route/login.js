1b5f42573611af67348fa146bd74045965fcbc5e
laithmohammedsaker <engr.laith@gmail.com>
2019-01-17 16:31:18 +0300

    add ? to url

--- route/login.js
+++ route/login.js
@@ -48,7 +48,7 @@ router.post('/',(req,res)=>{
                             if(target.permit === 'master'){ permit = 'master'; }
                             const token = jwt.sign({id : target.id,permit : permit}, secret)
                             res.cookie('X-auth-token',token);
-                            res.redirect(`${Params.originApp}/${token}`)
+                            res.redirect(`${Params.originApp}/?${token}`)
                         }else{ // if passwords are not matched, redirect to login page and tell app there are some errors
                             error.push('incorrect information, try again !!');
                             // res.send(error)
